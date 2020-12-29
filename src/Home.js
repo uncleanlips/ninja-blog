2ac2df47a2ed5834d5932285664b6bb34e2dbb53
charitraheen <shubhampaitrik8@gmail.com>
2020-12-29 16:46:34 +0530

    Adding Styles

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="home">
-      <h2>Home Page</h2>
+      <h2>Homepage</h2>
     </div>
   );
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,22 @@
 const Navbar = () => {
-    return ( 
-        <nav className="navba">
-            <h1>The Ninja Blog</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
-            </div>
-        </nav>
-     );
-}
- 
+  return (
+    <nav className="navbar">
+      <h1>The Dojo Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a
+          href="/create"
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          New Blog
+        </a>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
